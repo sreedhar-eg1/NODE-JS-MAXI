c56a959b9304fe95060b1c4d5fc2b530a2b0c465
sreedhar eg <sreedhareg1997@gmail.com>
2026-01-01 14:58:29 +0530

    Redirecting requests

---
+++ 01-Basics/app.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log("Listening");
+  //   console.log(req);
+
+  //   Some important request properties
+  console.log("Method:", req.method);
+  console.log("URL:", req.url);
+  console.log("Headers:", req.headers);
+
+  const url = req.url;
+  const method = req.method;
+
+  // To close the server after response
+  //   process.exit();
+
+  //   To set headers to the response
+  res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    // Writing response body
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+
+    // To tell that response is completed
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    // Writing file synchronously
+    fs.writeFileSync("message.txt", "DUMMY");
+
+    // individully setting status code and headers
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    // Alternative approach using writeHead to set statuscode and headers together
+    res.writeHead(302, { Location: "/" });
+
+    return res.end();
+  }
+
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
