225a576206084fc5aa8e51df89eaea2e0dbc1ab7
sreedhar eg <sreedhareg1997@gmail.com>
2026-01-13 10:30:47 +0530

    Handling Different Routes

--- 03 Working with Express.js/app.js
+++ 03 Working with Express.js/app.js
@@ -1,20 +1,26 @@
-const express = require("express")
+const express = require("express");
 
 // Create an Express application
-const app = express()
+const app = express();
 
 // To add new middleware, use app.use()
 app.use((req, res, next) => {
-    console.log("In one middleware")
-    next(); // Call next() to pass control to the next middleware
-})
+  console.log("In one middleware");
+  next(); // Call next() to pass control to the next middleware
+});
 
-app.use((req, res, next) => {
-    console.log("In another middleware")
+// Adding route to the middleware chain
+app.use("/home", (req, res, next) => {
+  console.log("In the /home middleware");
+  res.send("<h1>Welcome to the Home Page!</h1>");
+});
 
-    // send() ends the request-response cycle
-    res.send("<h1>Hello from Express.js!</h1>")
-})
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+
+  // send() ends the request-response cycle
+  res.send("<h1>Hello from Express.js!</h1>");
+});
 
 // Will create an HTTP server and listen on port 3000
 app.listen(3000);
