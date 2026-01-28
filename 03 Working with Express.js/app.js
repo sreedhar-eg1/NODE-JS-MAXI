5a21e949b09e87e9dee89c6efabc7c6225466d29
sreedhar eg <sreedhareg1997@gmail.com>
2026-01-28 10:42:36 +0530

    Parsing Incoming Requests

--- 03 Working with Express.js/app.js
+++ 03 Working with Express.js/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // Create an Express application
 const app = express();
+
+// Middleware to parse URL-encoded bodies (from HTML forms)
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // To add new middleware, use app.use()
 app.use((req, res, next) => {
@@ -14,6 +18,20 @@ app.use("/home", (req, res, next) => {
   console.log("In the /home middleware");
   res.send("<h1>Welcome to the Home Page!</h1>");
 });
+
+app.use("/add-product", (req, res, next) => {
+  console.log("add product middleware")
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>',
+  );
+});
+
+// with use, it will match all HTTP methods (GET, POST, etc.)
+app.use("/product", (req, res, next) => {
+  // To getform data
+  console.log(req.body) // undefined without body-parser or express.json() middleware
+  res.redirect("/");
+})
 
 app.use("/", (req, res, next) => {
   console.log("In another middleware");
