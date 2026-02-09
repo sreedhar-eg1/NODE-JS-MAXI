78bb13ab752e64372a12b395177377990b57b2b1
sreedhar eg <sreedhareg1997@gmail.com>
2026-02-09 14:10:31 +0530

    Creating & Managing a Dummy User

--- 08 Understanding Sequelize/app.js
+++ 08 Understanding Sequelize/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const errorController = require("./controllers/errors");
 
@@ -18,18 +20,41 @@ app.set("views", "views"); // Specify the views directory
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Adding associations
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+// User.hasMany(Product)
+
 // It basically creates the tables in the database if they don't exist and syncs
 // the models with the database. It returns a promise, so we can chain a .then()
 // to it to start the server after the database is synced.
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
-    // console.log(result)
+    return User.findByPk(1);
+    app.listen(3000);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Max", email: "test@test.com" });
+    }
+
+    return Promise.resolve(user);
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err)); // Sync the models with the database
