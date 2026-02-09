5dc868c7fca8e899c381fa5b842a049c9b26ad9c
sreedhar eg <sreedhareg1997@gmail.com>
2026-02-09 10:30:34 +0530

    Defining a Model

--- 08 Understanding Sequelize/models/product.js
+++ 08 Understanding Sequelize/models/product.js
@@ -1,33 +1,28 @@
-const db = require("../util/database");
+const { Sequelize } = require("sequelize");
 
-const Cart = require("./cart");
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+// Define a model for the  product table and its structure
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    // using prepared statements to prevent SQL injection
-    // VALUES (?, ?, ?, ?) are placeholders for the actual values
-    // which will be provided in the array as the second argument to the execute method
-    return db.execute(
-      "INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.description, this.imageUrl],
-    );
-  }
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id])
-  }
-
-  static deleteById(id) {}
-};
+module.exports = Product;
