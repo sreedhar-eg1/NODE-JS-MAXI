6971caa0b8165b801726e85f581f173b4d6d9797
sreedhar eg <sreedhareg1997@gmail.com>
2026-02-16 10:13:46 +0530

    working with cart and loading cart details

--- 10 Working with mongoose/controllers/shop.js
+++ 10 Working with mongoose/controllers/shop.js
@@ -40,14 +40,15 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) =>
+    .populate("cart.items.productId", "")
+    .then((user) => {
+      const products = user.cart.items;
       res.render("shop/cart", {
         pageTitle: "Your Cart",
         path: "/cart",
         products: products,
-      }),
-    )
+      });
+    })
     .catch((err) => console.log(err));
 };
 
