c004c1b0de27b141c3d3a6ceca2db731ffc4e57e
sreedhar eg <sreedhareg1997@gmail.com>
2026-02-20 11:00:29 +0530

    Setup & Basic Validation

--- 14 Understanding Validation/controllers/auth.js
+++ 14 Understanding Validation/controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 const user = require("../models/user");
@@ -66,6 +67,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array(),
+    });
+  }
 
   User.findOne({ email: email })
     .then((userDoc) => {
