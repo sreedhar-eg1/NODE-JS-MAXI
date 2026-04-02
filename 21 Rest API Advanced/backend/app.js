2ef1ab05f113edece2d3792db8fa469cd088aa91
sreedhar eg <sreedhareg1997@gmail.com>
2026-04-02 11:35:28 +0530

    Uploading Images

--- 21 Rest API Advanced/backend/app.js
+++ 21 Rest API Advanced/backend/app.js
@@ -1,25 +1,73 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const filestorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: filestorage, fileFilter: fileFilter }).single(
+  "image",
+);
+
 // app.use(bodyParser.urlencoded({extended: false})); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(upload);
 
 // Handling CORS Errors
 app.use((req, res, next) => {
-  res.setHeader("Acess-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   next();
 });
 
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sreedhareg1997_db_user:eT6lQe9C74f65Jpq@node-complete.ra50bsw.mongodb.net/messages",
+  )
+  .then((result) => {
+    app.listen(8080, () =>
+      console.log("Connected to Database and running on port 8080"),
+    );
+  })
+  .catch((err) => console.log(err));
