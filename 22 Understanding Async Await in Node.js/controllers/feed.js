790280a1d854e811ba36fd19edc4383b571364bd
sreedhar eg <sreedhareg1997@gmail.com>
2026-04-11 09:42:19 +0530

    completed async await request

--- 22 Understanding Async Await in Node.js/controllers/feed.js
+++ 22 Understanding Async Await in Node.js/controllers/feed.js
@@ -6,34 +6,29 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const User = require("../models/user");
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
 
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Post.find()
-        .populate("creator", "name")
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((posts) => {
-      res.status(200).json({
-        message: "Posts fetched successfully",
-        posts: posts,
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
+  try {
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .populate("creator", "name")
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
 
-      next(err);
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts: posts,
+      totalItems: totalItems,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+
+    next(err);
+  }
 };
 
 exports.createPost = (req, res, next) => {
