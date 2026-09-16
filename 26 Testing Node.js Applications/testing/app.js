00673ad60c3aa1de6aac3e2b174182ad8fd9e0bf
sreedhar eg <sreedhareg1997@gmail.com>
2026-09-16 15:07:55 +0530

    Testing Node.js Applications: Setup and Writing a First Test

---
+++ 26 Testing Node.js Applications/testing/app.js
@@ -0,0 +1,75 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+const filestorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: filestorage, fileFilter: fileFilter }).single(
+  "image",
+);
+
+// app.use(bodyParser.urlencoded({extended: false})); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(upload);
+
+// Handling CORS Errors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sreedhareg1997_db_user:eT6lQe9C74f65Jpq@node-complete.ra50bsw.mongodb.net/messages",
+  )
+  .then((result) => {
+    app.listen(8080, () =>
+      console.log("Connected to Database and running on port 8080"),
+    );
+  })
+  .catch((err) => console.log(err));
---
+++ 26 Testing Node.js Applications/testing/test/start.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+
+it("should add number correctly", function () {
+  const num1 = 3;
+  const num2 = 4;
+
+  expect(num1 + num2).to.equal(7);
+});
