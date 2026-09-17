2cc68cd113734773ef4f1c0c642c921ce82b2c06
sreedhar eg <sreedhareg1997@gmail.com>
2026-09-17 12:26:23 +0530

    Testing Node.js Applications: Organizing Multiple Tests

--- 26 Testing Node.js Applications/testing/test/auth-middleware.js
+++ 26 Testing Node.js Applications/testing/test/auth-middleware.js
@@ -2,15 +2,29 @@ const { expect } = require("chai");
 
 const authMiddleware = require("../middleware/is-auth");
 
-it("Should throw an error if no Authorization header is present", function () {
-  const req = {
-    get: function (headername) {
-      return null;
-    },
-  };
+// describe from mocha allows us to group multiple test
+// Basically it helps us to organize
+describe("Auth Middleware", function () {
+  it("Should throw an error if no Authorization header is present", function () {
+    const req = {
+      get: function (headername) {
+        return null;
+      },
+    };
 
-  //   We are binding this because we want chai to call this
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated. No token provided.",
-  );
+    //   We are binding this because we want chai to call this
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated. No token provided.",
+    );
+  });
+
+  it("Should throw an error if Authorization header is a single string", function () {
+    const req = {
+      get: function (headerName) {
+        return "sometoken";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
