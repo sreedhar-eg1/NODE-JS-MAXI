8ddb3722837d2247c0512a38aef50d6cbabc6c1d
sreedhar eg <sreedhareg1997@gmail.com>
2026-09-24 14:24:41 +0530

    Modern JavaScript & NodeJS: Node Core Modules & Promises

--- 28 Modern JavaScript & NodeJS/ES MODULE/resHandler.js
+++ 28 Modern JavaScript & NodeJS/ES MODULE/resHandler.js
@@ -1,17 +1,26 @@
 // const fs = require("fs");
-import fs from "fs";
+// import fs from "fs";
+import fs from "fs/promises";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const _fileName = fileURLToPath(import.meta.url); // Will give path to the current file
 const __dirname = dirname(_fileName); // gives path to the current folder
 
-const resHandler = (req, res, next) => {
+const resHandler = async (req, res, next) => {
   //   fs.readFile("my-page.html", "utf-8", (err, data) => {
   //     res.send(data);
   //   });
 
-  res.sendFile(path.join(__dirname, "my-page.html"));
+  //   Making use of promises in code Node Api
+  try {
+    const data = await fs.readFile("my-page.html", "utf-8");
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+
+  //   res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 // module.exports = resHandler;
