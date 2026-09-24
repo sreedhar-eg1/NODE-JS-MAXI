ac2e90418abcb6d52e268f6b073333d5a9f35f56
sreedhar eg <sreedhareg1997@gmail.com>
2026-09-24 14:16:17 +0530

    Modern JavaScript & NodeJS: More on ES Modules

--- 28 Modern JavaScript & NodeJS/ES MODULE/resHandler.js
+++ 28 Modern JavaScript & NodeJS/ES MODULE/resHandler.js
@@ -1,10 +1,17 @@
 // const fs = require("fs");
 import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const _fileName = fileURLToPath(import.meta.url); // Will give path to the current file
+const __dirname = dirname(_fileName); // gives path to the current folder
 
 const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf-8", (err, data) => {
-    res.send(data);
-  });
+  //   fs.readFile("my-page.html", "utf-8", (err, data) => {
+  //     res.send(data);
+  //   });
+
+  res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 // module.exports = resHandler;
